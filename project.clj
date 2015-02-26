6c2014c624d95cf9a1e92e0d1dabb36ca181dd28
Ruediger Gad <rgad@fb2.fh-frankfurt.de>
2015-02-26 12:26:59 +0100

    Add FIFO writer.

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.cli "0.2.4"]
+                 [fg-netzwerksicherheit/clj-jms-activemq-toolkit "1.99.0"]
+                 [clj-assorted-utils "1.9.0-SNAPSHOT"]]
+  :main clj-jms-activemq-fifo-writer.main)
