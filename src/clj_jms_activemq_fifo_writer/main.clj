9fb91cbc70ab25f562a317e14c1d30233d994489
Ruediger Gad <rgad@fb2.fh-frankfurt.de>
2015-07-23 11:07:57 +0200

    Add license and author information.

--- src/clj_jms_activemq_fifo_writer/main.clj
+++ src/clj_jms_activemq_fifo_writer/main.clj
@@ -1,4 +1,14 @@
-(ns clj-jms-activemq-fifo-writer.main
+;;;
+;;;   Copyright 2015, Frankfurt University of Applied Sciences
+;;;
+;;;   This software is released under the terms of the Eclipse Public License 
+;;;   (EPL) 1.0. You can find a copy of the EPL at: 
+;;;   http://opensource.org/licenses/eclipse-1.0.php
+;;;
+
+(ns
+  ^{:author "Ruediger Gad",
+    :doc "Main class"}
   (:use clojure.pprint
         [clojure.string :only [join split]]
         clojure.tools.cli
